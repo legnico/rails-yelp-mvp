143d9d16c0a58bfd90d0014441d33395a8c150b3
Nicolas Leguillon <legnico75@gmail.com>
2019-05-20 15:05:37 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create([{name: "Epicure", address: "1 rue de la juiverie", category:"french"},
+  {name: "Basilic & Co", address: "20 rue de Strasbourg", category:"italian"},
+  {name: "SushiShop", address: "15 rue de Strasbourg", category:"japanese"},
+  {name: "La Grande Muraille", address: "3 rue des Petites Ecuries", category:"chinese"},
+  {name: "La Ripaille", address: "10 Rue du Château", category:"beligian"}])
+
+# Review.create([{content: "Super bon resto !", rating: 5, restaurant_id: },
+#   {content: "Les meilleurs pizzas du quartier", rating: 5, restaurant: restaurants(1)},
+#   {content: "Franchement très moyen par rapport au prix", rating: 2, restaurant: restaurants(2)},
+#   {content: "Problèmes de digestion suite au repas", rating: 0, restaurant: restaurants(3)},
+#   {content: "Très bon rapport qualité/prix et bien servi en plus de ça", rating: 5, restaurant: restaurants.last}])
